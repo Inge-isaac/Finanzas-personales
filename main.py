33b0905beb8d7ea70f10d8e99b708c80143b3baf
Inge-isaac <Ing_isaac.corona@outlook.com>
2026-02-02 22:44:05 -0600

    UPDATE- Se parametrizo modulo ventana ingreso como modal, impedir cerrar master

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import tkinter as tk
-from tkinter import *
-from src.ventanas import ventana_ingresos, ventana_fondo_emergencias
+#from tkinter import *
+from src.ventanas import ventana_fondo_emergencias
+from src.ventana_ingresos import ventana_registro_ingresos as vri
 from src.dimensiones import center_window as cw
-from data.conexion import get_db_connection
+
 
 
  
@@ -15,8 +16,8 @@ window.resizable(False, False)
 #Dimensiones
 window.geometry("")
 #background
-window.config(bg="black")
-cw(window,700, 600) #Ejecuta el metodo del modulo dimensiones
+window.config(bg="skyblue")
+cw(window,600, 720) #Ejecuta el metodo del modulo dimensiones
 
 #Widgets de la ventana principal
 
@@ -28,10 +29,10 @@ window.config(menu=menubar)
 
 #Crear submenu
 file_menu = tk.Menu(menubar, tearoff=0, bg="darkblue", fg="white") #fg(foreground, afecta el corlor de la fuente /texto)
-menubar.add_cascade(label="Acción", menu=file_menu)
+menubar.add_cascade(label="Acción", menu=file_menu, foreground="white")
 
 #Agregar comandos al submenu Accion
-file_menu.add_command(label="Ingresos", command=ventana_ingresos)
+file_menu.add_command(label="Ingresos", command=lambda: vri(window))
 file_menu.add_command(label="Fondo de emergencia", command=ventana_fondo_emergencias)
 file_menu.add_command(label="Ahorro", command=None)
 file_menu.add_command(label="Gastos de alimentación", command=None)
@@ -40,5 +41,6 @@ file_menu.add_separator()
 file_menu.add_command(label="Salir", command=window.quit)
 
 
+if __name__ == "__main__":
+    tk.mainloop()
 
-tk.mainloop()
