32176833bdd568011e72728e9a5139f58b297ccc
Inge-isaac <Ing_isaac.corona@outlook.com>
2026-02-12 09:23:25 -0600

    MODIFY: estructura, se agrego en una clase

--- src/ventana_transacciones.py
+++ src/ventana_transacciones.py
@@ -1,238 +1,214 @@
-import os
 import tkinter as tk
-import pyodbc
 from tkinter import messagebox, ttk as tkk
-from data.conexion import get_db_connection
-from utils.center_windows import center_window as cw
-from utils.placeholder import PlaceholderEntry  as phe
-
-#Imporaciones para graficar
+import pyodbc
 import pandas as pd
-import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
-
-"Configuracion de la conexion a la base de datos"
-
-conn_str = get_db_connection()
+from data.conexion import get_db_connection
+from utils.center_windows import center_window as cw
 
 
-#fUNCION PARA REGISTRAR INGRESOS
-def registrar_ingreso():
-    acount = entry_account.get()
-    amount = entry_amount.get()
-    transaction = entry_transaction.get()
-    description = entry_description.get()
-    category = entry_category.get()
-    created = entry_created.get()
-    
-    
-    if not acount or not amount or not category or not created or not transaction or not description:
-        messagebox.showerror("Error", "Por favor, complete todos los campos.")
-        return
-    
-    try:
-        conn = pyodbc.connect(conn_str, timeout=5)
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO transactions (account_id, amount, transaction_date, description, category, created_at) VALUES (?, ?, ?, ?, ?, ?)", (acount, amount, transaction, description, category, created))
-        conn.commit()
-        messagebox.showinfo("Éxito", "Ingreso registrado correctamente.")
+class VentanaRegistrarTransaccion(tk.Toplevel):
+
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.title("Registrar Transaccion")
+        cw(self, 400, 500)
+        self.config(bg="lightsteelblue")
+        self.grab_set()
+
+        self.conn_str = get_db_connection()
+
+        self.crear_widgets()
+
+    # =============================
+    # CREAR INTERFAZ
+    # =============================
+    def crear_widgets(self):
+
+        tk.Label(self, bg="steelblue", fg="white", font=("Arial", 12),
+                 text="Id de la Cuenta:").grid(row=0, column=0, sticky="e", padx=5, pady=5)
+        self.entry_account = tk.Entry(self)
+        self.entry_account.grid(row=0, column=1, pady=5, sticky="w")
+
+        tk.Label(self, bg="steelblue", fg="white", font=("Arial", 12),
+                 text="Registro de monto:").grid(row=1, column=0, sticky="e", padx=5, pady=5)
+        self.entry_amount = tk.Entry(self)
+        self.entry_amount.grid(row=1, column=1, pady=5, sticky="w")
+
+        tk.Label(self, bg="steelblue", fg="white", font=("Arial", 12),
+                 text="Fecha de transacción:").grid(row=2, column=0, sticky="e", padx=5, pady=5)
+        self.entry_transaction = tk.Entry(self)
+        self.entry_transaction.grid(row=2, column=1, pady=5, sticky="w")
+
+        tk.Label(self, bg="steelblue", fg="white", font=("Arial", 12),
+                 text="Descripción:").grid(row=3, column=0, sticky="e", padx=5, pady=5)
+        self.entry_description = tk.Entry(self)
+        self.entry_description.grid(row=3, column=1, pady=5, sticky="w")
+
+        tk.Label(self, bg="steelblue", fg="white", font=("Arial", 12),
+                 text="Categoría:").grid(row=4, column=0, sticky="e", padx=5, pady=5)
+        self.entry_category = tk.Entry(self)
+        self.entry_category.grid(row=4, column=1, pady=5, sticky="w")
+
+        tk.Label(self, bg="steelblue", fg="white", font=("Arial", 12),
+                 text="Fecha creación:").grid(row=5, column=0, sticky="e", padx=5, pady=5)
+        self.entry_created = tk.Entry(self)
+        self.entry_created.grid(row=5, column=1, pady=5, sticky="w")
+
+        tk.Button(self, text="Registrar Transacción",
+                  command=self.registrar_ingreso).grid(row=6, column=0, columnspan=2, pady=10)
+
+        tk.Button(self, text="Actualizar Lista",
+                  command=self.mostrar_registros).grid(row=7, column=0, pady=5)
+
+        tk.Button(self, text="Ver Gráfico",
+                  command=self.ventana_grafico).grid(row=7, column=1, pady=5)
+
+        # ===== TABLA =====
+        frame_tabla = tk.Frame(self)
+        frame_tabla.grid(row=8, column=0, columnspan=2, sticky="nsew", padx=10, pady=10)
+        # ==== PERMITIR EXPANCION ====
+        self.grid_rowconfigure(8, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+        scroll_y = tk.Scrollbar(frame_tabla, orient="vertical")
+        scroll_y.pack(side="right", fill="y")
         
-        # Limpiar los campos después de registrar
-        entry_account.delete(0, tk.END)
-        entry_amount.delete(0, tk.END)
-        entry_transaction.delete(0, tk.END)
-        entry_description.delete(0, tk.END)
-        entry_category.delete(0, tk.END)
-        entry_created.delete(0, tk.END)
+        # ==== Scrollbar horizontal (opcional)
+        scroll_x = tk.Scrollbar(frame_tabla, orient="horizontal")
+        scroll_x.pack(side="bottom", fill="x")
+
+        self.tabla = tkk.Treeview(
+            frame_tabla,
+            columns=("ID_Acount", "Monto", "Fecha", "Descripción", "Categoría", "Creado"),
+            show="headings",
+            yscrollcommand=scroll_y.set,
+            xscrollcommand=scroll_x.set
+        )
+
+        self.tabla.heading("ID_Acount", text="ID Cuenta")
+        self.tabla.heading("Monto", text="Monto")
+        self.tabla.heading("Fecha", text="Fecha")
+        self.tabla.heading("Descripción", text="Descripción")
+        self.tabla.heading("Categoría", text="Categoría")
+        self.tabla.heading("Creado", text="Fecha Creación")
         
-    except Exception as e:
-        messagebox.showerror("Error", f"No se pudo registrar el ingreso: {e}")
-    
-    finally:
-        if 'conn' in locals() and conn:
-            conn.close()
-            
+        self.tabla.column("ID_Acount", width=60, stretch=False)
+        self.tabla.column("Monto", width=80, stretch=False)
+        self.tabla.column("Fecha", width=120, stretch=False)
+        self.tabla.column("Descripción", width=150, stretch=False)
+        self.tabla.column("Categoría", width=100, stretch=False)
+        self.tabla.column("Creado", width=120, stretch=False)
+       
+        self.tabla["displaycolumns"] = (
+        "ID_Acount", "Monto", "Fecha",
+        "Descripción", "Categoría", "Creado"
+)
 
-import os
-import pyodbc
-from tkinter import messagebox
-
-# ...existing code...
-
-def mostrar_registros():
-    # Limpiar tabla para evitar duplicados al refrescar
-    for fila in tabla.get_children():
-        tabla.delete(fila)
+        self.tabla.pack(fill="both", expand=True)
         
- #Funcion mostrar registros desde la base de datos
-    conn = pyodbc.connect(conn_str, timeout=5)
-    if conn:
-        cursor = conn.cursor()
+        scroll_y.config(command=self.tabla.yview)
+        scroll_x.config(command=self.tabla.xview)
+
+    # =============================
+    # REGISTRAR
+    # =============================
+    def registrar_ingreso(self):
+
+        datos = (
+            self.entry_account.get(),
+            self.entry_amount.get(),
+            self.entry_transaction.get(),
+            self.entry_description.get(),
+            self.entry_category.get(),
+            self.entry_created.get()
+        )
+
+        if not all(datos):
+            messagebox.showerror("Error", "Complete todos los campos")
+            return
+
         try:
-            cursor.execute("SELECT account_id, amount, transaction_date, description, category, created_at FROM transactions")
-            filas = cursor.fetchall()
-            
-            for fila in filas:
-                # Insertar al final de la tabla
-                tabla.insert("", "end", values=(fila[0], fila[1], fila[2], fila[3], fila[4], fila[5]))
-                if fila[1] < 0:
-                    tabla.item(tabla.get_children()[-1], tags=("negativo",))
-                    tabla.tag_configure("negativo", foreground="orangered", font=("Arial", 8, "bold"))
-                elif fila[1] > 0:
-                    tabla.item(tabla.get_children()[-1], tags=("positivo",))
-                    tabla.tag_configure("positivo", foreground="limegreen", font=("Arial", 8, "bold"))
-            
-              
+            conn = pyodbc.connect(self.conn_str, timeout=5)
+            cursor = conn.cursor()
+
+            cursor.execute("""
+                INSERT INTO transactions
+                (account_id, amount, transaction_date, description, category, created_at)
+                VALUES (?, ?, ?, ?, ?, ?)
+            """, datos)
+
+            conn.commit()
+            messagebox.showinfo("Éxito", "Ingreso registrado")
+
+            self.limpiar_campos()
+            self.mostrar_registros()
+
         except Exception as e:
-            print(f"Error al consultar: {e}")
+            messagebox.showerror("Error", str(e))
+
         finally:
             conn.close()
 
+    # =============================
+    def limpiar_campos(self):
+        self.entry_account.delete(0, tk.END)
+        self.entry_amount.delete(0, tk.END)
+        self.entry_transaction.delete(0, tk.END)
+        self.entry_description.delete(0, tk.END)
+        self.entry_category.delete(0, tk.END)
+        self.entry_created.delete(0, tk.END)
 
+    # =============================
+    # CONSULTAR
+    # =============================
+    def mostrar_registros(self):
 
-def ventana_grafico():
-    # Conexión
-    conn = pyodbc.connect('DRIVER={SQL Server};SERVER=EL-ZURDO;DATABASE=personal_finances;UID=isaa;PWD=123456')
-    df = pd.read_sql("SELECT amount, category FROM transactions", conn)
-    conn.close()
-    
-    root = tk.Tk()
-    root.title("Gráfico DB")
+        for fila in self.tabla.get_children():
+            self.tabla.delete(fila)
 
-    # Crear figura
-    fig = Figure(figsize=(9, 5), dpi=100)
-    ax = fig.add_subplot(111)
-    ax.bar(df['category'], df['amount']) # Ejemplo gráfico
+        try:
+            conn = pyodbc.connect(self.conn_str, timeout=5)
+            cursor = conn.cursor()
 
-    # Cambia solo el tamaño de las etiquetas del eje X
-    ax.tick_params(axis='x', labelsize=6, labelcolor='blue', ) 
-    # Embeber en Tkinter
-    canvas = FigureCanvasTkAgg(fig, master=root)
-    canvas.draw()
-    canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)         
+            cursor.execute("SELECT account_id, amount, transaction_date, description, category, created_at FROM transactions")
+            filas = cursor.fetchall()
 
+            for f in filas:
+                item = self.tabla.insert("", "end", values=(f[0], f[1], f[2], f[3], f[4], f[5]))
 
-def ventana_registrar_transaccion(master):
-    global entry_account, entry_amount, entry_transaction
-    global entry_description, entry_category, entry_created
-    global tabla
+                if f[1] < 0:
+                    self.tabla.item(self.tabla.get_children()[-1], tags=("negativo",))
+                else:
+                    self.tabla.item(item, tags=("positivo",))
 
-    
-    #Interfaz grafica para registrar ingresos
-    root = tk.Toplevel(master)
-    root.title("Registrar Transaccion")
-    cw(root, 400, 500)
-    root.config(bg="lightsteelblue")
-    root.grab_set()  # Hacer que la ventana sea modal
-    
+            self.tabla.tag_configure("negativo", foreground="orangered")
+            self.tabla.tag_configure("positivo", foreground="limegreen")
 
-    #Widgets
-    # Configuración con GRID
-    # El Label va en column=0 y el Entry en column=1
-    tk.Label(root, bg="steelblue", fg="white", font=("Arial", 12), text="Id de la Cuenta:").grid(row=0, column=0, sticky="e", padx=5, pady=5)
-    entry_account = tk.Entry(root)
-    entry_account.grid(row=0, column=1, pady=5, sticky="w")
+        except Exception as e:
+            print(e)
+        finally:
+            conn.close()
 
-    tk.Label(root,bg="steelblue", fg="white", font=("Arial", 12), text="Registro de monto:").grid(row=1, column=0, sticky="e",columnspan=1, padx=5, pady=5)
-    entry_amount = tk.Entry(root)
-    entry_amount.grid(row=1, column=1, pady=5, sticky="w")
-    tk.Label(root,bg="steelblue", fg="white", font=("Arial", 12), text="Fecha de transacción:").grid(row=2, column=0, sticky="e", padx=5, pady=5)
-    entry_transaction = tk.Entry(root)
-    entry_transaction.grid(row=2, column=1, pady=5, sticky="w")
+    # =============================
+    # GRAFICO
+    # =============================
+    def ventana_grafico(self):
 
-    tk.Label(root,bg="steelblue", fg="white", font=("Arial", 12), text="Descripción de transacción:").grid(row=3, column=0, sticky="e", padx=5, pady=5)
-    entry_description = tk.Entry(root)
-    entry_description.grid(row=3, column=1, pady=5, sticky="w")
+        conn = pyodbc.connect(self.conn_str)
+        df = pd.read_sql("SELECT amount, category FROM transactions", conn)
+        conn.close()
 
-    tk.Label(root,bg="steelblue", fg="white", font=("Arial", 12), text="Categoría de transacción:").grid(row=4, column=0, sticky="e", padx=5, pady=5)
-    entry_category = tk.Entry(root)
-    entry_category.grid(row=4, column=1, pady=5, sticky="w")
+        top = tk.Toplevel(self)
+        top.title("Gráfico")
 
-    tk.Label(root,bg="steelblue", fg="white", font=("Arial", 12), text="Fecha (YYYY-MM-DD):").grid(row=5, column=0, sticky="e", padx=5, pady=5)
-    entry_created = tk.Entry(root)
-    entry_created.grid(row=5, column=1, pady=5, sticky="w")
+        fig = Figure(figsize=(7, 4))
+        ax = fig.add_subplot(111)
+        ax.bar(df["category"], df["amount"])
 
-    btn_registrar = tk.Button(root, text="Registrar Transacción", command=registrar_ingreso)
-    btn_registrar.grid(row=6, column=0, columnspan=2, pady=20)
-
-
-    btn_consultar = tk.Button(root, text="Actualizar Lista", command=mostrar_registros)
-    btn_consultar.grid(row=7, column=0, columnspan=2, pady=10)
-    
-    btn_mostrar_grafico = tk.Button(root, text="Ver Gráfico", command=ventana_grafico)
-    btn_mostrar_grafico.grid(row=7, column=1, columnspan=1, pady=10)
-    
-    # ===== FRAME PARA LA TABLA =====
-    frame_tabla = tk.Frame(root)
-    frame_tabla.grid(row=8, column=0, columnspan=2, sticky="nsew", padx=10, pady=10)
-
-    # Permitir expansión
-    root.grid_rowconfigure(8, weight=1)
-    root.grid_columnconfigure(1, weight=1)
-
-    # Scrollbar vertical
-    scroll_y = tk.Scrollbar(frame_tabla, orient="vertical")
-    scroll_y.pack(side="right", fill="y")
-
-    # Scrollbar horizontal (opcional)
-    scroll_x = tk.Scrollbar(frame_tabla, orient="horizontal")
-    scroll_x.pack(side="bottom", fill="x")
-
-    # Treeview
-    tabla = tkk.Treeview(
-        frame_tabla,
-        columns=("ID_Acount", "Monto", "Fecha Transacción", "Descripción", "Categoría", "Fecha Creación"),
-        show="headings",
-        yscrollcommand=scroll_y.set,
-        xscrollcommand=scroll_x.set
-    )
-    
-    # Configurar columnas
-    tabla.column("ID_Acount", anchor="w", width=50)     
-    tabla.column("Monto", anchor="center", width=70)   
-    tabla.column("Fecha Transacción", anchor="center", width=120)
-    tabla.column("Descripción", anchor="w", width=100)
-    tabla.column("Categoría", anchor="center", width=100)
-    tabla.column("Fecha Creación", anchor="center", width=120)
-    # Configurar encabezados
-    tabla.heading("ID_Acount", text="ID Cuenta", anchor="w")
-    tabla.heading("Monto", text="Monto")
-    tabla.heading("Fecha Transacción", text="Fecha Transacción")
-    tabla.heading("Descripción", text="Descripción")
-    tabla.heading("Categoría", text="Categoría")
-    tabla.heading("Fecha Creación", text="Fecha Creación")
-
-    tabla.pack(fill="both", expand=True)
-
-    # Conectar scrollbars
-    scroll_y.config(command=tabla.yview)
-    scroll_x.config(command=tabla.xview)
-
-    
-    #root.mainloop()
-    # Botón para activar la función
-
-    #ventana_registro_ingresos()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        canvas = FigureCanvasTkAgg(fig, master=top)
+        canvas.draw()
+        canvas.get_tk_widget().pack(fill="both", expand=True)
